6231d2109cdf9cb209456db08272ed98a6f6c563
Lê Hải Long <32591564+HLong179@users.noreply.github.com>
2018-06-29 11:45:56 +0700

    Update db.js

--- fn/db.js
+++ fn/db.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 exports.load = sql => {
     return new Promise((resolve, reject) => {
         var cn = mysql.createConnection({
-            host: 'localhost',
-            //port: 3306,
-            user: 'root',
-            password: '11111',
-            database: 'wine',
+           // host: 'localhost',
+            port: 3306,
+            user: 'sql3245172',
+            password: '9xZW9l5v3F',
+            database: 'sql3245172',
             //insecureAuth: true
         });
 
@@ -28,10 +28,11 @@ exports.load = sql => {
 exports.save = sql => {
     return new Promise((resolve, reject) => {
         var cn = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '11111',
-            database: 'wine',
+           // host: 'localhost',
+            port: 3306,
+            user: 'sql3245172',
+            password: '9xZW9l5v3F',
+            database: 'sql3245172',
             insecureAuth: true
         });
 
